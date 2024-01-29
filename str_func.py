c4953eaed2dfd2abbf005c2afe291c5f9815bbe2
Danila <maevepro28@mail.ru>
2024-01-29 03:01:14 +0300

    add new func capital_letter and docstring

--- str_func.py
+++ str_func.py
@@ -1,3 +1,11 @@
 def capitalize_string():
+    """Функция которая делает заглавными все
+       буквы каждого слова в строке """
     string = input()
     return print(string.upper())
+
+def capital_letter():
+    """Функция которая делает заглавными первые
+    буквы каждого слова в строке """
+    string = input()
+    return print(string.title())
